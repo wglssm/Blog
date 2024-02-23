531089186b8348248a01c1972b4da635709db262
Lesi Wei <wglssm.wls@gmail.com>
2024-02-23 20:25:16 +1030

    update

---
+++ jsAdvance/DataStructure.js
@@ -0,0 +1,14 @@
+console.log("1111");
+
+// difference between undefined and null
+
+var a;
+console.log(a); // undefined
+a = null;
+console.log(a); // null
+
+var c = function () {}; // object
+//C store the address of {}
+console.log(typeof c); //;
+
+//Data: store in the memory location as 0101010
